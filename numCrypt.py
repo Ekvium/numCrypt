4c8966cdaee9479c12e646f92cfef248a88bcce7
Dmitry Mycelin <Mycelin@users.noreply.github.com>
2016-04-24 12:50:29 +0500

    Variable key length

--- numCrypt.py
+++ numCrypt.py
@@ -1,9 +1,10 @@
 def numCrypt(num, reverse = false, digits = 8):
-	key = 'b*2m*tSB081y|e~4paxxMgl#TAAApE@F'
+	key = list('b*2m*tSB081y|e~4paxxMgl#TAAApE@F')
+	keylen = len(key)
 	num = list(str(num).rjust(digits, '0'))
 	iterations = 3 # don't use 1
 	reverse = -1 if reverse else 1
 	for i in range(0, iterations):
 		for d in range(0, digits):
-			num[d] = (num[d] + 300 + (ord(key[(i+d)%32]) + num[(d+digits-1)%digits]) * reverse) % 10;
+			num[d] = (num[d] + 300 + (ord(key[(i+d)%keylen]) + num[(d+digits-1)%digits]) * reverse) % 10;
 	return ''.join(num);
